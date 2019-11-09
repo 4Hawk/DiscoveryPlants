760c051bddd0a7d3a619b69af9e9d68f4b6251e5
Shamil Rivera Acacio <reyiv19@gmail.com>
2019-11-09 10:47:28 -0500

    DbPlantasM

--- Models/Plantas.cs
+++ Models/Plantas.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using DiscoveryPlants_1.Models;
 
 namespace DiscoveryPlants.Models
 {
@@ -7,10 +8,17 @@ namespace DiscoveryPlants.Models
         public int PlantaId { get; set; }
         [Required]
         public string Nombre { get; set; }
+
+        public string Usos { get; set; }
+        [Required]
+        public string Caracteristicas { get; set; }
         [Required]
         public string Foto{get;set;}
-        [Required]
-        public string Descripcion{get;set;}
+
+        public string Ubicacion { get; set; }
+
+        public Categorias CategoriaAsig { get; set; }
+        public string InfoAdicional{get;set;}
         public int CategoriaId { get; set; }
     }
 }
